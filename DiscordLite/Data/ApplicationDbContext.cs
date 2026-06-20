3c7eec97700a96787b357c1cb6596e23b4b13dfe
Daviti Kutibashvili <82726216+DavitKutibashvili@users.noreply.github.com>
2026-06-20 17:30:15 +0400

    Delete DiscordLite directory

--- DiscordLite/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace DiscordLite.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
-    {
-    }
-}
--- DiscordLite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DiscordLite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
